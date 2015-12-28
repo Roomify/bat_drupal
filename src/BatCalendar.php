cc0f7ceee0b63a7fa140fd7bb5078891095dd201
istos <ronald@roomify.us>
2015-12-28 12:02:25 +0100

    RD-1: BatCalendars should always have a store and set of units defined

--- src/BatCalendar.php
+++ src/BatCalendar.php
@@ -13,7 +13,7 @@ namespace Drupal\bat;
  */
 class BatCalendar extends BatAbstractCalendar {
 
-  public function __construct($store = array(), $unit_ids = array()) {
+  public function __construct($unit_ids, $store) {
     $this->unit_ids = $unit_ids;
     $this->store = $store;
   }
