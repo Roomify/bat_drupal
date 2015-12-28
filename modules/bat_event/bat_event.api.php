2baaa02ae28e8f0f94d030af1dbbef25fb337931
Nicolò Caruso <nicolo@istos.it>
2015-12-28 18:38:14 +0100

    RD-1: Add 'hook_bat_facets_search_results_alter'

--- modules/bat_event/bat_event.api.php
+++ modules/bat_event/bat_event.api.php
@@ -8,27 +8,8 @@
  */
 
 /**
- * Allows to modify the price modifiers array for a event.
  *
- * @param array $price_modifiers
- *   Array containing the event price modifiers.
- * @param array $event_info
- *   Array containing the event information. Contains the following key/value
- *   pairs:
- *   - start_date: DateTime object containing the event start date.
- *   - end_date: DateTime object containing the event end date. In this case,
- *   the end date represents the last night the unit is blocked, so it is one
- *   day before the checkout date entered in the event form.
- *   - unit: The BatUnit entity the event is related to.
- *   - event_parameters: Array containing some other event parameters:
- *     - group_size: The total number of persons included in the event.
  */
-function hook_bat_price_modifier_alter(&$price_modifiers, $event_info) {
-  // This adds programmatically a 10$ discount.
-  $price_modifiers['my_module'] = array(
-    '#type' => BAT_DYNAMIC_MODIFIER,
-    '#quantity' => 1,
-    '#op_type' => BAT_SUB,
-    '#amount' => 10
-  );
+function hook_bat_facets_search_results_alter(&$units, $context) {
+	unset($units[0]);
 }
