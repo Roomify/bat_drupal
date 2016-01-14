f7eff08acc91bed0160e71f2b4ef38ea462d2dc3
Nicolò Caruso <nicolo@istos.it>
2016-01-14 15:20:53 +0100

    Move type_id field and filter to bat_unit

--- modules/bat_event_ui/src/FullCalendarOpenStateEventFormatter.php
+++ modules/bat_event_ui/src/FullCalendarOpenStateEventFormatter.php
@@ -45,6 +45,7 @@ class FullCalendarOpenStateEventFormatter extends AbstractEventFormatter {
       'start' => $event->startYear() . '-' . $event->startMonth('m') . '-' . $event->startDay('d') . 'T' . $event->startHour('H') . ':' . $event->startMinute() . ':00Z',
       'end' => $event->endYear() . '-' . $event->endMonth('m') . '-' . $event->endDay('d') . 'T' . $event->endHour('H') . ':' . $event->endMinute() . ':00Z',
       'title' => $bat_unit->formatEventValue($this->event_type->type, $default_value),
+      'blocking' => 0,
       'fixed' => 0,
     );
 
