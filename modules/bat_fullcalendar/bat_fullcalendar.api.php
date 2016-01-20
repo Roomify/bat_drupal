89c614a14a5dc6f6893be6a42395b053b5250363
Nicolò Caruso <nicolo@istos.it>
2016-01-20 15:24:20 +0100

    Document hooks in Bat fullcalendar

---
+++ modules/bat_fullcalendar/bat_fullcalendar.api.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @file
+ * This file contains no working PHP code; it exists to provide additional
+ * documentation for doxygen as well as to document hooks in the standard
+ * Drupal manner.
+ */
+
+/**
+ * Allow other modules to alter calendar settings.
+ *
+ * @param $calendar_settings
+ */
+function hook_bat_calendar_settings_alter(&$calendar_settings) {
+
+}
+
+/**
+ * Provide alter hook to change calendar js files.
+ *
+ * @param $js_files
+ */
+function hook_bat_fullcalendar_render_js_alter(&$js_files) {
+
+}
+
+/**
+ * Provide alter hook to change calendar css files.
+ *
+ * @param $css_files
+ */
+function hook_bat_fullcalendar_render_css_alter(&$css_files) {
+
+}
+
+/**
+ * Allow other modules to change the modal style.
+ *
+ * @param $modal_style
+ */
+function hook_bat_fullcalendar_modal_style(&$modal_style) {
+
+}
