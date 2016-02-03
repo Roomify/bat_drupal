1e91e852a06fb8c927535de80f1638dec4850aa6
Adrian Rollett <acrollet@gmail.com>
2016-02-03 16:43:14 -0700

    Update bat events to be able to reference any entity type

--- modules/bat_event/bat_event.api.php
+++ modules/bat_event/bat_event.api.php
@@ -12,5 +12,19 @@
  * @param $context
  */
 function hook_bat_facets_search_results_alter(&$units, $context) {
-	unset($units[0]);
+  unset($units[0]);
+}
+
+/**
+ * Allow modules to define entity types that may be referenced by Bat Events
+ * and provided to the BAT library as a Unit.
+ *
+ * NB: Entity types returned by this hook must have a
+ * getEventDefaultValue method.
+ *
+ * @return array $entity_types
+ *   Single-dimensional array with machine names of entity types.
+ */
+function hook_bat_event_target_entity_types() {
+  return(array('bat_unit'));
 }
