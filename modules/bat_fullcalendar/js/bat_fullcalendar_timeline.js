b597489f8b7e0b897e9e28a03ad444481f87e8f5
Adrian Rollett <acrollet@gmail.com>
2016-02-03 18:30:08 -0700

    Updates to bat_api

--- modules/bat_fullcalendar/js/bat_fullcalendar_timeline.js
+++ modules/bat_fullcalendar/js/bat_fullcalendar_timeline.js
@@ -157,7 +157,7 @@ function saveBatEvent(event, revertFunc, calendars) {
   var unit_id = event.resourceId.substring(1);
 
   // Retrieve all events for the unit and time we're dragging onto.
-  var events_url = '/bat/v2/events?unit_ids=' + unit_id + '&start_date=' + event.start.format('YYYY-MM-DD HH:mm') +
+  var events_url = '/bat/v2/events?target_ids=' + unit_id + '&target_entity_type=bat_unit&start_date=' + event.start.format('YYYY-MM-DD HH:mm') +
                    '&end_date=' + event.end.format('YYYY-MM-DD HH:mm') + '&event_types=' + event.type;
   proceed = true;
   jQuery.ajax({
