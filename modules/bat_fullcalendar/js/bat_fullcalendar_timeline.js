1b47e7875df320409c58fa64bf8c528ee06556ef
Nicolò Caruso <nicolo@istos.it>
2016-02-05 12:41:18 +0100

    Pass 'minTime' and 'maxTime' as settings

--- modules/bat_fullcalendar/js/bat_fullcalendar_timeline.js
+++ modules/bat_fullcalendar/js/bat_fullcalendar_timeline.js
@@ -34,6 +34,8 @@ Drupal.behaviors.bat_event = {
         businessHours: Drupal.settings.batCalendar[0].businessHours,
         defaultView: Drupal.settings.batCalendar[0].defaultView,
         selectConstraint: Drupal.settings.batCalendar[0].selectConstraint,
+        minTime: Drupal.settings.batCalendar[0].minTime,
+        maxTime: Drupal.settings.batCalendar[0].maxTime,
         views: {
           timelineDay: {
             buttonText: Drupal.settings.batCalendar[0].viewsTimelineDayButtonText,
