1a0d62dd8fa8eceb575b5a1f8addd53014b134fb
Nicolò Caruso <nicolo@istos.it>
2016-05-17 11:01:37 +0200

    Add 'No charge' option

---
+++ modules/bat_options/bat_options.api.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * This file contains no working PHP code; it exists to provide additional
+ * documentation for doxygen as well as to document hooks in the standard
+ * Drupal manner.
+ */
+
+function hook_bat_options_price_options_alter(&$options) {
+  // No example.
+}
