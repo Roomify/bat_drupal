6882caa7b2bda193f469e99f7f036d975e760cbd
Nicolò Caruso <nicolo@istos.it>
2016-10-14 15:33:54 +0200

    Improve ‘BAT Booking Example’ module

---
+++ modules/bat_booking/bat_booking_example/src/Controller/BatBookingExampleController.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\bat_booking_example\Controller\BatBookingExampleController.
+ */
+
+namespace Drupal\bat_booking_example\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+
+class BatBookingExampleController extends ControllerBase implements ContainerInjectionInterface {
+
+  /**
+   * Constructs a BatBookingExampleController object.
+   */
+  public function __construct() {
+  }
+
+  public function batBookingConfirmationPage($start_date, $end_date, $type_id) {
+    $header = $start_date->format('Y-m-d') . ' - ' . $end_date->format('Y-m-d');
+    $form = \Drupal::formBuilder()->getForm('Drupal\bat_booking_example\Form\BookingConfirmationForm', $start_date, $end_date, $type_id);
+
+    return array(
+      '#theme' => 'booking_confirmation_page',
+      '#header' => $header,
+      '#form' => $form,
+    );
+  }
+
+}
