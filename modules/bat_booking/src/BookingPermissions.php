9b50d0245098e151406292faab579e623a265e3c
Nicolò Caruso <nicolo@istos.it>
2018-04-06 10:41:21 +0200

    Improve Booking permissions

--- modules/bat_booking/src/BookingPermissions.php
+++ modules/bat_booking/src/BookingPermissions.php
@@ -46,6 +46,17 @@ class BookingPermissions implements ContainerInjectionInterface {
   public function permissions() {
     $permissions = [];
 
+    $permissions += bat_entity_access_permissions('bat_booking');
+
+    foreach (bat_booking_get_bundles() as $bundle_name => $bundle_info) {
+      $permissions['view own booking entities of bundle ' . $bundle_name] = [
+        'title' => t('View own %bundle @entity_bundle', ['@entity_bundle' => 'bookings', '%bundle' => $bundle_info->label()]),
+      ];
+      $permissions['view any booking entity of bundle ' . $bundle_name] = [
+        'title' => t('View any %bundle @entity_bundle', ['@entity_bundle' => 'booking', '%bundle' => $bundle_info->label()]),
+      ];
+    }
+
     return $permissions;
   }
 
